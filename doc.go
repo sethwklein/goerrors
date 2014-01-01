6d2668133a8e51acaeebab8e782a7fb5f282eb26
Seth W. Klein <sk@sethwklein.net>
2014-01-01 01:14:00 -0500

    Add deprecation notice
    Please use sethwklein.net/go/errutil instead. It provides the same
    features, but without the duplication of the standard errors package.

--- doc.go
+++ doc.go
@@ -1,5 +1,9 @@
 // Package errors provides things for working with single and multiple errors.
 //
+// This package has been deprecated. Please use sethwklein.net/go/errutil
+// instead. It provides the same features, but without the duplication of
+// the standard errors package.
+//
 // It is a drop in replacement for the standard errors package
 // but with extra features for handling multiple errors
 // and correctly handling errors from deferred calls.
